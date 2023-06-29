019706d43c62ec64237068dba7a768b6789db179
Радченко Катерина <ketrinradchenko@gmail.com>
2023-06-29 12:32:15 +0300

    Add project files.

---
+++ dz_serialization_2/Program.cs
@@ -0,0 +1,131 @@
+﻿using NLog;
+using NLog.Conditions;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+namespace dz_serialization_2
+{
+    internal class Program
+    {
+        public static Logger Logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            #region NLog Initializator
+            var config = new NLog.Config.LoggingConfiguration();
+
+            //-------------------- Target Console ----------------------------------------
+
+            var consoleTarget = new ColoredConsoleTarget()
+            {
+                Layout = @"${longdate}|${level:uppercase=true}|${logger}|${message}"
+            };
+
+            // Rules for mapping loggers to targets
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, consoleTarget);
+
+            //-------------------- Target File ----------------------------------------
+            // Apply config
+            NLog.LogManager.Configuration = config;
+           
+
+            var Trace = new ConsoleRowHighlightingRule();
+            Trace.Condition = ConditionParser.ParseExpression("level == LogLevel.Trace");
+            Trace.ForegroundColor = ConsoleOutputColor.Yellow;
+            consoleTarget.RowHighlightingRules.Add(Trace);
+            var Debug = new ConsoleRowHighlightingRule();
+            Debug.Condition = ConditionParser.ParseExpression("level == LogLevel.Debug");
+            Debug.ForegroundColor = ConsoleOutputColor.DarkCyan;
+            consoleTarget.RowHighlightingRules.Add(Debug);
+            var Info = new ConsoleRowHighlightingRule();
+            Info.Condition = ConditionParser.ParseExpression("level == LogLevel.Info");
+            Info.ForegroundColor = ConsoleOutputColor.Green;
+            consoleTarget.RowHighlightingRules.Add(Info);
+            var Warn = new ConsoleRowHighlightingRule();
+            Warn.Condition = ConditionParser.ParseExpression("level == LogLevel.Warn");
+            Warn.ForegroundColor = ConsoleOutputColor.DarkYellow;
+            consoleTarget.RowHighlightingRules.Add(Warn);
+            var Error = new ConsoleRowHighlightingRule();
+            Error.Condition = ConditionParser.ParseExpression("level == LogLevel.Error");
+            Error.ForegroundColor = ConsoleOutputColor.DarkRed;
+            consoleTarget.RowHighlightingRules.Add(Error);
+            var Fatal = new ConsoleRowHighlightingRule();
+            Fatal.Condition = ConditionParser.ParseExpression("level == LogLevel.Fatal");
+            Fatal.ForegroundColor = ConsoleOutputColor.Black;
+            Fatal.BackgroundColor = ConsoleOutputColor.DarkRed;
+            consoleTarget.RowHighlightingRules.Add(Fatal);
+
+            #endregion NLog Colors
+
+            string nameFile = "Mytxt.txt";
+            string myText = "THE 123.ААА a";
+
+            RecordFile(myText, nameFile);
+            DataFiltering(nameFile);
+
+            Console.ReadLine();
+        }
+        static void DataFiltering(string nameFile)
+        {
+            try
+            {
+                Logger.Info("Начало фильтрации");
+                Logger.Info($"{nameFile} открыт");
+
+                string content = File.ReadAllText(nameFile);
+                string[] sentences = content.Split('.');
+
+                Console.WriteLine("Отобразить все предложения, содержащие хотя бы одну маленькую, английскую букву");
+                foreach (string sentence in sentences)
+                {
+                    if (sentence.Any(char.IsLower))
+                    {
+                        Console.WriteLine(sentence);
+                    }
+                }
+
+                Console.WriteLine("Отобразить все предложения, содержащие хотя бы одну цифру");
+                foreach (string sentence in sentences)
+                {
+                    if (sentence.Any(char.IsDigit))
+                    {
+                        Console.WriteLine(sentence);
+                    }
+                }
+
+                Console.WriteLine("Отобразить все предложения, содержащие хотя бы одну большую, английскую букву");
+                foreach (string sentence in sentences)
+                {
+                    if (sentence.Any(char.IsUpper))
+                    {
+                        Console.WriteLine(sentence);
+                    }
+                }
+
+                Logger.Info($"{nameFile} закрыт");
+            }
+            catch (FileNotFoundException)
+            {
+                Logger.Warn("Файл не найден");
+            }
+        }
+        static void RecordFile(string myText, string nameFile)
+        {
+            try
+            {
+                Logger.Info($"{nameFile} открыт");
+
+                using (StreamWriter fileWriter = new StreamWriter(nameFile))
+                {
+                    Logger.Info("Запись в файл");
+                    fileWriter.Write(myText);
+                }
+
+                Logger.Info($"{nameFile} закрыт");
+            }
+            catch (FileNotFoundException)
+            {
+                Logger.Warn("Файл не найден");
+            }
+        }
+    }
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Net;
+using System.Numerics;
+using System.Xml.Linq;
+using NLog.Fluent;
+using NLog;
+using NLog.Targets;
+using System.Diagnostics;
+using NLog.Conditions;
+
+namespace task_2
+{
+
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+
+    class Program
+    {
+        public static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            User user1 = new User
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Phone = "123-456-7890",
+                Email = "john.doe@example.com",
+                Address = "123 Main Street"
+            };
+
+            User user2 = new User
+            {
+                FirstName = "Jane",
+                LastName = "Smith",
+                Phone = "987-654-3210",
+                Email = "jane.smith@example.com",
+                Address = "456 Elm Street"
+            };
+
+            User user3 = new User
+            {
+                FirstName = "Bob",
+                LastName = "Johnson",
+                Phone = "555-123-4567",
+                Email = "bob.johnson@example.com",
+                Address = "789 Oak Avenue"
+            };
+
+            List<User> fakeUser = new List<User>();
+            fakeUser.Add(user1);
+            fakeUser.Add(user2);
+            fakeUser.Add(user3);
+
+            var config = new NLog.Config.LoggingConfiguration();
+
+            //-------------------- Target Console ----------------------------------------
+
+            var consoleTarget = new ColoredConsoleTarget()
+            {
+                Layout = @"${longdate}|${level:uppercase=true}|${logger}|${message}"
+            };
+
+            // Rules for mapping loggers to targets
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, consoleTarget);
+
+            //-------------------- Target File ----------------------------------------
+            // Apply config
+            NLog.LogManager.Configuration = config;
+
+            var Trace = new ConsoleRowHighlightingRule();
+            Trace.Condition = ConditionParser.ParseExpression("level == LogLevel.Trace");
+            Trace.ForegroundColor = ConsoleOutputColor.Yellow;
+            consoleTarget.RowHighlightingRules.Add(Trace);
+            var Debug = new ConsoleRowHighlightingRule();
+            Debug.Condition = ConditionParser.ParseExpression("level == LogLevel.Debug");
+            Debug.ForegroundColor = ConsoleOutputColor.DarkCyan;
+            consoleTarget.RowHighlightingRules.Add(Debug);
+            var Info = new ConsoleRowHighlightingRule();
+            Info.Condition = ConditionParser.ParseExpression("level == LogLevel.Info");
+            Info.ForegroundColor = ConsoleOutputColor.Green;
+            consoleTarget.RowHighlightingRules.Add(Info);
+
+            Logger.Info($"Старт генерации пользователей");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Logger.Debug($"Сгенерирован пользователь: @{fakeUser[i].FirstName}");
+            }
+
+            Logger.Info("Генерация фейковых пользователей завершена");
+
+            Console.ReadLine();
+        }
+    }
+}
+
